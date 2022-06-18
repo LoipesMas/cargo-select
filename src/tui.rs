9e30e9bb36591fe960e784c8d241c8d4473c9aba
LoipesMas <46327403+LoipesMas@users.noreply.github.com>
2022-06-18 13:43:15 +0200

    `CTRL+W` to delete a word in TUI

--- src/tui.rs
+++ src/tui.rs
@@ -54,6 +54,15 @@ impl Tui {
                 {
                     return Err("User interrupt.".into());
                 }
+                if matches!(key.code, KeyCode::Char('w'))
+                    && key.modifiers.contains(KeyModifiers::CONTROL) {
+                        // delete last word
+                        let mut p = pattern.pop();
+                        while !matches!(p, Some(' ') | None){ 
+                            p = pattern.pop();
+                        }
+                        continue;
+                }
                 match key.code {
                     KeyCode::Char(c) => pattern.push(c),
                     KeyCode::Backspace => {
